ade4ebfcfd02717ef9f20ffcaa58f28e31b357d0
Dmitry Markvart <goodmanboxe@yandex.ru>
2020-12-23 20:07:06 +0300

    add string Buy!

--- src/main/java/com/markvart/task/FirstTask.java
+++ src/main/java/com/markvart/task/FirstTask.java
@@ -2,6 +2,8 @@ package com.markvart.task;
 
 public class FirstTask {
     public static void main(String[] args) {
-
+        System.out.println("Hello world!");
+        for(int i =0; i<8; i++)
+            System.out.println("Buy!");
     }
 }
